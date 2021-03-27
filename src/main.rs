c3aed5348d7d52f9543e21ed87db4198ce2b6e0f
Krakaw <41575888+Krakaw@users.noreply.github.com>
2021-03-27 07:43:34 +0200

    Adding Tari crypto lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use rand::rngs::OsRng;
+use serde::{Deserialize, Serialize};
+use tari_crypto::keys::PublicKey;
+use tari_crypto::ristretto::{RistrettoPublicKey, RistrettoSecretKey};
+#[derive(Deserialize)]
+pub struct FreeYatRequest {
+    username: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SignResult {
+    pub public_nonce: Option<String>,
+    pub signature: Option<String>,
+    pub error: String,
+}
+
+impl Default for SignResult {
+    fn default() -> Self {
+        SignResult {
+            public_nonce: None,
+            signature: None,
+            error: "".into(),
+        }
+    }
+}
+
+async fn sign_request(data: web::Json<FreeYatRequest>) -> Result<HttpResponse> {
+    let mut result: SignResult = SignResult::default();
+
+    Ok(HttpResponse::Ok().json(&result))
+}
+
+struct AppState {
+    app_name: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    eprintln!(
+        "RistrettoPublicKey::random_keypair(&mut OsRng) = {:?}",
+        RistrettoPublicKey::random_keypair(&mut OsRng)
+    );
+    HttpServer::new(|| {
+        App::new().route("/sign", web::post().to(sign_request))
+        // .data(AppState {
+        //     app_name: String::from("Yat"),
+        // })
+        // .service(web::scope("/code").route("/hey", web::get().to(manual_hello)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
